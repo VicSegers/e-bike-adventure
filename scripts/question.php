014e15bee8339c6957c4dd59d8af797660b2d634
Vic Segers <vic.segers10@gmail.com>
2021-02-10 12:53:22 +0100

    Headers test

--- scripts/question.php
+++ scripts/question.php
@@ -1,18 +1,24 @@
 <?php
-function spamcheck($field) {
-  $field = filter_var($field, FILTER_SANITIZE_EMAIL);
-  return filter_var($field, FILTER_VALIDATE_EMAIL);
-}
+  function spamcheck($field) {
+    $field = filter_var($field, FILTER_SANITIZE_EMAIL);
+    return filter_var($field, FILTER_VALIDATE_EMAIL);
+  }
 
-if ($_POST['email'] && spamcheck($_REQUEST['email'])) {
-  $email = $_REQUEST['email']; 
-  $subject = $_REQUEST['subject'];
-  $message = $_REQUEST['message'];
+  if ($_POST['email'] && spamcheck($_REQUEST['email'])) {
+    $email = $_REQUEST['email']; 
+    $subject = $_REQUEST['subject'];
+    $message = $_REQUEST['message'];
 
-  // mail("info@e-bikeadventure.be", "Subject: [Via website] $subject", $message, "From: $email" );
-  mail("vic.segers10@gmail.com", "Subject: [Via website] $subject", $message, "From: $email");
-  echo readfile("../response/success.html")[0];
-} else {
-  echo readfile("../response/error.html")[0];
-}
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type: text/html; charset=iso-8859-1" . "\r\n";
+    $headers .= "From: $email" . "\r\n" .
+    "Reply-To: $email" . "\r\n" .
+    "X-Mailer: PHP/" . phpversion();
+
+    // mail("info@e-bikeadventure.be", "Subject: [Via website] $subject", $message, "From: $email" );
+    mail("vic.segers10@gmail.com", "Subject: [Via website] $subject", $message, $headers);
+    echo readfile("../response/success.html")[0];
+  } else {
+    echo readfile("../response/error.html")[0];
+  }
 ?>
